0a4fcf0408522ea85b97a76626235fb6aa3c03e6
IT21168390 <it21168390@my.sliit.lk>
2024-04-04 15:26:54 +0530

    Init

---
+++ src/main/java/com/axcertro_demo_crud/AxcertroDemoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.axcertro_demo_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxcertroDemoCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxcertroDemoCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/axcertro_demo_crud/AxcertroDemoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axcertro_demo_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxcertroDemoCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
